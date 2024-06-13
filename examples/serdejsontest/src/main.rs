69acd3da1e787fbfe8c0e7431e3750de54d15c6c
hunjixin <root@ip-172-31-44-171.ap-southeast-1.compute.internal>
2024-06-13 07:37:45 +0000

    examples: add serde example

---
+++ examples/serdejsontest/src/main.rs
@@ -0,0 +1,52 @@
+#![feature(trait_alias)]
+
+
+use std::hash::Hash;
+use serde::{de::DeserializeOwned, Serialize, Deserialize};
+
+pub trait GID =  Eq + Clone + Copy + Hash + Serialize + for<'de> Deserialize<'de>;
+
+
+
+pub trait Unit<ID> where ID:GID {
+    fn id(&self) -> ID;
+    fn name(&self) -> String;
+}
+
+
+// Channel use to definite data transfer channel
+#[derive(Serialize, Deserialize, Debug,Clone)]
+pub struct Channel {
+}
+
+
+// ComputeUnit used to define logic for data generation, transformer, ouput
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ComputeUnit<ID>
+where ID:GID {
+    #[serde(bound(deserialize = ""))]
+    pub id: ID,
+
+    pub name: String,
+    pub channel: Option<Channel>,
+
+    pub image: String,
+    pub cmd: Vec<String>,
+
+    #[serde(bound(deserialize = ""))]
+    pub(crate) dependency: Vec<ID>,
+}
+
+impl<ID:GID> Unit<ID> for ComputeUnit<ID> {
+    fn id(&self) -> ID {
+        self.id
+    }
+
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
